86c144ec64f857161e245f2e4db41335cf3961a5
MINT <pb1123love@gmail.com>
2024-06-07 13:19:33 +0900

    add movie page

---
+++ app/components/movie-info.tsx
@@ -0,0 +1,13 @@
+import { API_URL } from "../(home)/page";
+
+async function getMovie(id: string) {
+  const response = await fetch(`${API_URL}/${id}`);
+  const json = response.json();
+  return json;
+}
+
+export default async function MovieInfo({ id }: { id: string }) {
+  const movie = await getMovie(id);
+
+  return <h6>{JSON.stringify(movie)}</h6>;
+}
