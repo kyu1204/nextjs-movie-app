68afd39f7f457c2b7a2acfa0074d841ca2be8545
MINT <pb1123love@gmail.com>
2024-06-08 00:43:08 +0900

    ✨ Add IMovie interface for type safety.

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,19 +1,25 @@
-import MovieInfo from "@/app/components/movie-info";
+import MovieInfo, { getMovie } from "@/app/components/movie-info";
 import MovieVideos from "@/app/components/movie-videos";
 import { Suspense } from "react";
 
-export default async function MovieDetail({
-  params: { id },
-}: {
-  params: { id: string };
-}) {
+interface IParams {
+  params: {
+    id: string;
+  };
+}
+export async function generateMetadata({ params: { id } }: IParams) {
+  const movie = await getMovie(id);
+  return {
+    title: movie.title,
+  };
+}
+
+export default async function MovieDetail({ params: { id } }: IParams) {
   return (
     <div>
-      <h3>Movie detail page</h3>
       <Suspense fallback={<h1>Loading movie info</h1>}>
         <MovieInfo id={id} />
       </Suspense>
-      <h4>Videos</h4>
       <Suspense fallback={<h1>Loading movie videos</h1>}>
         <MovieVideos id={id} />
       </Suspense>
--- app/components/movie-videos.tsx
+++ app/components/movie-videos.tsx
@@ -1,15 +1,28 @@
+import styles from "../styles/movie-video.module.css";
+
 import { API_URL } from "../(home)/page";
 
 async function getVideos(id: string) {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  throw new Error("someting broke.");
-  //   const response = await fetch(`${API_URL}/${id}/videos`);
-  //   const json = response.json();
-  //   return json;
+  // await new Promise((resolve) => setTimeout(resolve, 3000));
+  // throw new Error("someting broke.");
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  const json = response.json();
+  return json;
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
   const videos = await getVideos(id);
 
-  return <h6>{JSON.stringify(videos)}</h6>;
+  return (
+    <div className={styles.container}>
+      {videos.map((video: any) => (
+        <iframe
+          key={video.id}
+          src={`https://youtube.com/embed/${video.key}`}
+          title={video.name}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        ></iframe>
+      ))}
+    </div>
+  );
 }
